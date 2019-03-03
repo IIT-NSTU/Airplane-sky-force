9eb77158061cf3faf48136a6f8ebfb44cf7a3966
kamruzzaman <shekhnstuiit@gmail.com>
2019-03-04 01:27:41 +0600

    gams manager added

---
+++ gameManager.java
@@ -0,0 +1,19 @@
+package SkyForce_MainFrame;
+
+import java.awt.Graphics;
+
+public class gameManager {
+
+			
+				public void init() {
+					
+				}
+					public void tick() {
+										
+										
+									}
+				public void render(Graphics g) {
+					
+					
+				}
+}
