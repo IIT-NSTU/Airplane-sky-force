f735ef1ef553a3cb2c6c4f79fcec809cebf6cbeb
kamruzzaman <shekhnstuiit@gmail.com>
2019-03-03 23:52:44 +0600

    Main Class

---
+++ Main_Class.java
@@ -0,0 +1,11 @@
+package SkyForce_MainFrame;
+
+public class Main_Class {
+	private static  Game_Maintaining game;
+		public static void main(String[] x) {
+	    	game= new Game_Maintaining("Sky Force ",500,600);
+	    	game.start();
+	     }
+	
+   
+}
