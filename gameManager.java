caa0291fbc43182621e160aca032c0bbefc3a08c
KamruzzamanShekh <46775644+kamruzzamanshekh@users.noreply.github.com>
2019-03-04 21:16:43 +0600

    update

--- gameManager.java
+++ gameManager.java
@@ -3,17 +3,18 @@ package SkyForce_MainFrame;
 import java.awt.Graphics;
 
 public class gameManager {
-
-			
-				public void init() {
-					
-				}
-					public void tick() {
-										
-										
-									}
-				public void render(Graphics g) {
-					
-					
-				}
+	 private Player player;
+	
+	public void init() {
+                  player=new Player((Game_Maintaining.gameWidth/2)+40,(Game_Maintaining.gameHeight-30)+50);
+		  player.init();
+	}
+	
+       public void tick() {
+                      player.tick();
+	}
+	
+	public void render(Graphics g) {
+                     player.render(g);
+	}
 }
