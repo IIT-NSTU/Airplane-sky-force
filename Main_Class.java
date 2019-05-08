17a52f6e3c5e95c0e3aedd173b0b2515d18de44f
kamruzzaman <shekhnstuiit@gmail.com>
2019-05-08 23:09:40 +0600

    updated

--- Main_Class.java
+++ Main_Class.java
@@ -1,11 +1,23 @@
 package SkyForce_MainFrame;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.FileReader;
+import java.io.IOException;
+
+import javax.swing.JOptionPane;
+
 public class Main_Class {
-	private static  Game_Maintaining game;
+	private static int point;
+	 static StringBuilder stringbuilder;
+	public int getPrivousPoint() {
+		return point;
+	}
 		public static void main(String[] x) {
-	    	game= new Game_Maintaining("Sky Force ",500,600);
-	    	game.start();
-	     }
-	
-   
+	    	//game= new Game_Maintaining("Sky Force ",500,600);
+	    	//game.start();
+			Home home=new Home();
+    
+   }
 }
