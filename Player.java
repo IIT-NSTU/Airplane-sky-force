2efd20ee61e3fdd2990f769361647a8b34ae294c
kamruzzaman <shekhnstuiit@gmail.com>
2019-05-08 23:12:31 +0600

    add up and down  move

--- Player.java
+++ Player.java
@@ -4,19 +4,26 @@ import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
 
 public class Player implements KeyListener {
 	private int x;
 	private int y;
 	 private boolean left ,right;
+	 private boolean up,down;
 	 private boolean attack;
 	 private SkyForce_Frame display;
 	 private gameManager manager;
-	
+     private long present;
+     private long late;
+     private static int strength;
 	public Player(int x_point,int y_point) {
 		this.x=x_point;
 		this.y=y_point;
-		
 	}
 	public int getX() {
 		return x;
@@ -25,35 +32,57 @@ public class Player implements KeyListener {
 		return y;
 	}
 	public void init() {
+		present=System.nanoTime();
+		late=100;
+		strength=2;
 		display.frame.addKeyListener(this);
+		ImageClass.init();
 		
 	}
 	public void tick() {
-		
+		  if(!(strength<=0)){
 	if(left) {
 		if(x>50) {
-		x=x-3;
+		x=x-5;
 	}
-    }
+	}
 		
 	if(right) {
-		if(x<Game_Maintaining.gameWidth+50-30)
+		if(x<Game_Maintaining.gameWidth-5)
 		{
 	
 		x=x+3;
 	}
 	}
-		if(attack) {
-			
-			manager.bullet.add(new Bullet(x+15,y));
+	if(up) {
+		if(y>50) {
+			y=y-3;
 		}
-		
 	}
+	if(down) {
+		if(y<393) {
+			y+=5;
+		}
+	}
+		if(attack) {
+			long wait=(System.nanoTime()-present)/1000000;
+			if(wait>late) {
+			manager.bullet.add(new Bullet(x+25,y));
+		}
+			present=System.nanoTime();
+		}
+		  }
+	}
+	//}
+
 	public void render(Graphics g) {
-		g.setColor(Color.green);
-		g.fillRect(x, y, 30, 30);
-		
+		if(!(strength<=0)) {
+		//g.drawImage(ImageClass.player,x, y, 50, 50, null);
+			g.drawImage(ImageClass.player,x,y-5,60,60,null);
+		//}
 	}
+	}
+	
 	@Override
 	public void keyPressed(KeyEvent e) {
 		int key=e.getKeyCode();
@@ -63,11 +92,20 @@ public class Player implements KeyListener {
 		if(key==KeyEvent.VK_RIGHT) {
 			right=true;
 		}
-		if(key==KeyEvent.VK_SPACE   ) {
+		if(key==KeyEvent.VK_B) {
 			attack=true;
 		}
-		
+		if(key==KeyEvent.VK_SPACE) {
+			attack=true;
+		}
+		if(key==KeyEvent.VK_UP) {
+			up=true;
+		}
+		if(key==KeyEvent.VK_DOWN) {
+			down=true;
+		}
 	}
+	
 	@Override
 	public void keyReleased(KeyEvent e) {
 		int key=e.getKeyCode();
@@ -77,14 +115,29 @@ public class Player implements KeyListener {
 		if(key==KeyEvent.VK_RIGHT) {
 			right=false;
 		}
+		if(key==KeyEvent.VK_B) {
+			attack=false;
+		}
 		if(key==KeyEvent.VK_SPACE) {
 			attack=false;
+		}
+		if(key==KeyEvent.VK_UP) {
+			up=false;
+		}
+		if(key==KeyEvent.VK_DOWN) {
+			down=false;
 		}
 	}
 	@Override
 	public void keyTyped(KeyEvent arg0) {
 		// TODO Auto-generated method stub
 		
+	}
+	public void setStrength(int s) {
+		this.strength=s;
+	}
+	public int getStrength() {
+		return this.strength;
 	}
 	
          
