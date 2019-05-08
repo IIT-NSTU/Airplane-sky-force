d8fbf433fd52876d0fe560c3ddd98f8fd1ecb861
kamruzzaman <shekhnstuiit@gmail.com>
2019-05-08 23:07:19 +0600

    open background and enemies images

--- loadimage.java
+++ loadimage.java
@@ -1,38 +1,53 @@
-package gameproject;
+package SkyForce_MainFrame;
 
 import java.awt.image.BufferedImage;
 import java.io.IOException;
+import java.util.Random;
 
 import javax.imageio.ImageIO;
 
-public class loadimage {
-	public static BufferedImage image;
-	public static BufferedImage entities;
-	public static BufferedImage player,enemy;
-	public static void init () {
-		
-		image=imageLoader("/SkyForce.JPG");
-		entities=imageLoader("/Warplane.PNG");
-		
-		crop();
-	}
-	
-	
-	public static BufferedImage imageLoader(String path) {
-		 try {
-			return ImageIO.read(loadimage.class.getResource(path));
-		} catch (IOException e) {
-			
-			e.printStackTrace();
-			System.exit(1);
-			
-		}
-		 return null;
-	}
-	
-	public static void crop() {
-		enemy=entities.getSubimage(0, 0, 90,95);
-		player=entities.getSubimage(80, 0, 115,95);
-	}
+public class ImageClass {
 
+    public static BufferedImage image,bag1,bag2,bag3,bag4,bag5;
+    public static BufferedImage CombineImg;
+    public static BufferedImage player,enemy1,enemy2,enemy3,enemy4
+    ,enemy5,enemy6,enemy11,enemy22,enemy33,enemy44,enemy55,enemy66;
+    public static BufferedImage enemy=null;
+    private static gameManager manager;
+    public static void init()
+    {
+   // image=imageLoader("/background.jpg");
+    CombineImg=imageLoader("/sky-airplane.png");
+   // enemy=CombineImg.getSubimage(0, 0, 115, 95);
+    
+    //open enemies image
+    enemy1=imageLoader("/plane1.png");
+    enemy2=imageLoader("/plane2.png");
+    enemy3=imageLoader("/plane3.png");
+    enemy4=imageLoader("/plane4.png");
+    enemy5=imageLoader("/plane5.png");
+    enemy6=imageLoader("/plane6.png");
+    
+    //open background image
+    bag1=imageLoader("/background1.jpg");
+    bag2=imageLoader("/background2.jpg");
+    bag3=imageLoader("/background3.jpg");
+    bag4=imageLoader("/background4.jpg");
+    bag5=imageLoader("/background5.jpg");
+
+	 player=CombineImg.getSubimage(115, 0, 115, 95);
+    }
+   
+     public static BufferedImage imageLoader(String path){
+           try {
+           return 
+ImageIO.read(ImageClass.class.
+getResource(path));
+       } catch (IOException e) {
+           e.printStackTrace();
+           System.exit(1);
+       }
+           return null;     
+     }
+  
 }
