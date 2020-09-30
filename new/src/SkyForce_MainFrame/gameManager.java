e774fecd30debecc48f5e42852121e16d18f02c0
s96Abrar <s96mini.cube@gmail.com>
2020-10-01 00:25:51 +0600

    Refactored game manager class

--- new/src/SkyForce_MainFrame/gameManager.java
+++ new/src/SkyForce_MainFrame/gameManager.java
@@ -6,8 +6,13 @@ import java.util.ArrayList;
 import java.util.Random;
 
 public class gameManager {
-    public static ArrayList<Bullet> bullet;
-    public static ArrayList<Enemy> enemy;
+    public static final String NewScoreSavePath = "C:\\Users\\IIT\\newscore.txt";
+    public static int GameWidth = 500;
+    public static int GameHeight = 500;
+
+    public static ArrayList<Bullet> bulletList;
+    public static ArrayList<Enemy> enemyList;
+    
     public static int score = 0;
     public static int ex, ey;
     public static Game_Maintaining maintain;
@@ -17,183 +22,188 @@ public class gameManager {
     private static FileOutputStream file;
     private Player player;
     private Enemy eny;
-    private long current_time;
+    private long currentTime;
     private long late;
     private SkyForce_Frame f;
     private Home home;
-    private int strength;
-    private int label = 2;
+    private int playerStrength;
+    private int level = 2;
 
     public void init() {
-        player = new Player((Game_Maintaining.gameWidth / 2) + 40, (Game_Maintaining.gameHeight - 50) + 50);
+        int playerPosX = (GameWidth / 2) + 40;
+        int playerPosY = (GameHeight - 50) + 50;
+        
+        player = new Player(playerPosX, playerPosY);
+        bulletList = new ArrayList<Bullet>();
+        enemyList = new ArrayList<Enemy>();
         player.init();
-        bullet = new ArrayList<Bullet>();
-        enemy = new ArrayList<Enemy>();
-        current_time = System.nanoTime();
+        currentTime = System.nanoTime();
         late = 2000;
-        strength = player.getStrength();
+        playerStrength = player.getStrength();
     }
 
     public void tick() {
-
         player.tick();
-        for (int i = 0; i < bullet.size(); i++) {
-            bullet.get(i).tick();
-        }
-        long wait = (System.nanoTime() - current_time) / 1000000;
-        if (wait > late) {
+        for (int i = 0; i < bulletList.size(); i++) 
+            bulletList.get(i).tick();
 
-            for (int i = 0; i < label; i++) {
-                Random random = new Random();
-                int x_ran = random.nextInt(450);
-                int y_ran = random.nextInt(450);
-                if (strength > 0) {
+        generateRandomEnemy();
 
-                    enemy.add(new Enemy(x_ran, -y_ran));
-
-                }
-            }
-            current_time = System.nanoTime();
-        }
-
-        for (int i = 0; i < enemy.size(); i++) {
-            enemy.get(i).tick();
-        }
+        for (int i = 0; i < enemyList.size(); i++)
+            enemyList.get(i).tick();
     }
 
-    // starting render method
     public void render(Graphics g) {
-
         player.render(g);
-        for (int i = 0; i < bullet.size(); i++) {
-            if (bullet.get(i).getY() > 50) {
-                bullet.get(i).render(g);
-            } else {
-                bullet.remove(bullet);
-            }
-        }
-        for (int i = 0; i < enemy.size(); i++) {
-            if (enemy.get(i).getX() >= 55 && enemy.get(i).getX() <= 450 - 33 && enemy.get(i).getY() >= 52
-                    && enemy.get(i).getY() <= 450) {
-                enemy.get(i).render(g);
-            }
 
-        }
-        // bullet and enemy collision
-        for (int e = 0; e < enemy.size(); e++) {
-            int ex = enemy.get(e).getX();
-            int ey = enemy.get(e).getY();
-            this.ey = ey;
-            for (int b = 0; b < bullet.size(); b++) {
-                int bx = bullet.get(b).getX();
-                int by = bullet.get(b).getY();
+        renderBulletList(g);
+        renderEnemyList(g);
 
-                // if(ex<bx+6 && ex+40>bx && ey<by+10 && ey+40>by)
-                // if(ex<bx+6 && ex+40>bx && ey<by+10 && ey>by)
-                if (ey >= 55 && ey <= 450) {
-                    if (ex < bx + 10 && ex + 40 > bx && ey < by + 10 && ey + 44 > by) {
-                        enemy.remove(e);
-                        e--;
-                        bullet.remove(b);
-                        b--;
-                        score += 5;
-                    }
-                }
+        bulletDestroyingEnemy();
+        playerEnemyCollision();
 
-            }
-
-        }
-        // player and enemy collision
-
-        for (int e = 0; e < enemy.size(); e++) {
-            int ex = enemy.get(e).getX();
-            int ey = enemy.get(e).getY();
-
-            int px = player.getX();
-            int py = player.getY();
-
-            if (px < ex + 40 && px + 60 > ex && py < ey + 40 && py + 60 > ey) {
-//				enemy.removeAll(enemy);
-//				f.frame.dispose();
-//				home = new Home();
-//				home.frame.setVisible(true);
-                // finished();
-                enemy.remove(e);
-                strength--;
-                if (strength <= 0) {
-                    enemy.removeAll(enemy);
-                    player.setStrength(0);
-
-                    f.frame.dispose();
-                    score = 0;
-                    home = new Home();
-                    home.frame.setVisible(true);
-                    // maintain.stop();
-                }
-
-            }
-        }
         g.setColor(Color.BLUE);
         g.setFont(new Font("arial", Font.BOLD, 30));
         g.drawString("Score: " + score, 20, 550);
-        g.drawString("Life: " + strength, 320, 550);
+        g.drawString("Life: " + playerStrength, 320, 550);
         g.setColor(Color.RED);
         g.drawLine(50, 495, 450, 495);
+
         if (score % 100 == 0 && score != 0) {
             g.setColor(Color.GREEN);
             g.drawString("Congratulation " + score, 160, 40);
         }
+
         if (score >= 0 && score <= 99) {
-            label = 2;
+            level = 2;
             g.drawString("Level 1", 160, 40);
         } else if (score >= 100 && score <= 199) {
             try {
-                for (Bullet b : bullet) {
+                for (Bullet b : bulletList)
                     b.setMotion(6);
-                }
-
             } catch (Exception e) {
                 System.out.println(e.getMessage());
             }
             g.drawString("Level 2", 160, 40);
-            label = 5;
-
+            level = 5;
         } else if (score >= 200 && score <= 500) {
             try {
-                for (Bullet b : bullet) {
+                for (Bullet b : bulletList)
                     b.setMotion(8);
-                }
-
             } catch (Exception e) {
                 System.out.println(e.getMessage());
             }
             g.drawString("Level 3", 160, 40);
-            label = 7;
+            level = 7;
         } else if (score >= 501 && score <= 1000) {
             try {
-                for (Bullet b : bullet) {
+                for (Bullet b : bulletList)
                     b.setMotion(10);
-                }
-
             } catch (Exception e) {
                 System.out.println(e.getMessage());
             }
             g.drawString("Level 4", 160, 40);
-            label = 11;
+            level = 11;
         }
-        try {
-            file = new FileOutputStream("C:\\Users\\IIT\\newscore.txt");
 
+        saveScore();
+    }
+
+    private void generateRandomEnemy() {
+        long wait = (System.nanoTime() - currentTime) / 1000000;
+        if (wait > late) {
+            for (int i = 0; i < level; i++) {
+                Random random = new Random();
+                int randomX = random.nextInt(450);
+                int randomY = random.nextInt(450);
+                if (playerStrength > 0)
+                    enemyList.add(new Enemy(randomX, -randomY));
+            }
+            currentTime = System.nanoTime();
+        }
+    }
+
+    private void renderBulletList(Graphics g) {
+        for (Bullet bullet : bulletList) {
+            if (bullet.getPositionY() > 50)
+                bullet.render(g);
+            else
+                bulletList.clear();
+        }
+    }
+
+    private void renderEnemyList(Graphics g) {
+        for (int i = 0; i < enemyList.size(); i++) {
+            if (enemyList.get(i).getX() >= 55 &&
+                    enemyList.get(i).getX() <= 450 - 33 &&
+                    enemyList.get(i).getY() >= 52 &&
+                    enemyList.get(i).getY() <= 450) {
+                enemyList.get(i).render(g);
+            }
+        }
+    }
+
+    private void bulletDestroyingEnemy() {
+        for (Enemy enemy : enemyList) {
+            int enemyPosX = enemy.getX();
+            int enemyPosY = enemy.getY();
+
+            this.ey = enemyPosY;
+
+            for (Bullet bullet : bulletList) {
+                int bulletPosX = bullet.getPositionX();
+                int bulletPoxY = bullet.getPositionY();
+
+                if (enemyPosY >= 55 && enemyPosY <= 450) {
+                    if (enemyPosX < (bulletPosX + 10)
+                            && (enemyPosX + 40) > bulletPosX
+                            && enemyPosY < (bulletPoxY + 10)
+                            && (enemyPosY + 44) > bulletPoxY) {
+                        // Removing imediately from the list can cause exception
+                        enemyList.remove(enemy);
+                        bulletList.remove(bullet);
+                        score += 5;
+                    }
+                }
+            }
+        }
+    }
+
+    private void playerEnemyCollision() {
+        for (Enemy enemy : enemyList) {
+            int enemyPosX = enemy.getX();
+            int enemyPosY = enemy.getY();
+
+            int playerPosX = player.getX();
+            int playerPosY = player.getY();
+
+            if (playerPosX < (enemyPosX + 40)
+                    && (playerPosX + 60) > enemyPosX
+                    && playerPosY < (enemyPosY + 40)
+                    && (playerPosY + 60) > enemyPosY) {
+                enemyList.remove(enemy);
+                playerStrength--;
+                if (playerStrength <= 0) {
+                    enemyList.removeAll(enemyList);
+                    player.setStrength(0);
+                    score = 0;
+
+                    f.frame.dispose();
+                    Home home = new Home();
+                }
+            }
+        }
+    }
+
+    private void saveScore() {
+        try {
+            FileOutputStream file = new FileOutputStream(NewScoreSavePath);
             String s = Integer.toString(score);
             byte[] b = s.getBytes();
             file.write(b);
             file.close();
-
-        } catch (Exception e2) {
-            // TODO: handle exception
+        } catch (Exception e) {
+            e.printStackTrace();
         }
-
     }
-
-    // end render method
 }
